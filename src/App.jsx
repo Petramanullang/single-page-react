cb131c18909ea1e2e47ec499437c5221d2e8345e
Petramanullang <petra.mainacc@gmail.com>
2023-11-12 21:07:15 +0700

    add folder

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      <div className="">
+        <h1 className="text-blue-700">Single Page</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
